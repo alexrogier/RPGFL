cd91523fe46329659b88b69560ce2b71559661ac
alexrogier <alexrogier.webit@gmail.com>
2015-07-12 21:14:28 -0400

    web services commit

---
+++ RPGFL_WebServices/RPGFL_WebServices/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RPGFL_WebServices.Startup))]
+namespace RPGFL_WebServices
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
